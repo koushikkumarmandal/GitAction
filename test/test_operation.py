5b9caaf1de127812b6709d167253446908eb0b3e
Koushik Mandal <koushikmandal712122@gmail.com>
2026-09-08 11:01:12 +0530

    Fix pytest test discovery

--- test/test_operation.py
+++ test/test_operation.py
@@ -1,11 +1,11 @@
 from src.math import sum,mul
 
-def sum_test():
+def test_sum():
     assert sum(2,3)==5
     assert sum(5,6)==11
     assert sum(-5,8)==3
 
-def mul_test():
+def test_mul():
     assert mul(2,3)==6
     assert mul(5,6)==30
     assert mul(-5,-3)==15
